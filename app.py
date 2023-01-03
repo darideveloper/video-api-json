75f69f558405b480cd820096724c721aa066fbf8
darideveloper <darideveloper@gmail.com>
2023-01-02 20:52:07 -0600

    post film

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 
 # Start app
@@ -19,9 +19,20 @@ def get_films ():
     with open(JSON_PATH, 'r', encoding='UTF-8') as file:
         json_data = json.loads(file.read())
         return json_data["films"]
+    
+def save_films (new_films):
+    """ Update films in JSON file
+    """
+    
+    with open(JSON_PATH, 'r', encoding='UTF-8') as file:
+        json_data = json.loads(file.read())
+    
+    json_data["films"] = new_films
+    with open(JSON_PATH, 'w', encoding='UTF-8') as file:
+        file.write(json.dumps(json_data))
         
 @app.get('/<int:id>/')
-def index (id):
+def get_film (id):
     """ Get data from simgle film, using the position in the films list
 
     Args:
@@ -31,11 +42,37 @@ def index (id):
         dicctionary: data of the film or error message
     """
     films = get_films()
-    if films[id]:
-        return films[id]
-    else: 
-        return {"error": "Film not found"}, 400        
     
+    if films[id]:
+        # Return fil data if exist
+        return films[id], 200
+    else: 
+        # Return error if film not exist
+        return {"error": "Film not found"}, 400  
+    
+@app.post('/')
+def post_film ():
+    """ Save new film in JSON file
+
+    Returns:
+        dicctionary: confirmation or error message
+    """
+    
+    films = get_films()
+    
+    # Get new film data from json
+    new_film = request.get_json()
+    
+    if new_film in films:
+        # return error if film already exists
+        return {"error": "Film already exists"}, 400
+    else:
+        # Save fil data in json file
+        films.append (new_film)
+        save_films (films)        
+        
+        # return confirmation message
+        return {"message": "Film added"}, 200
 
 if __name__ == '__main__':
     app.run(debug=True)
